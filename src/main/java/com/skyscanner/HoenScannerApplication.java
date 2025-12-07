548bc7c2911cdde876c3b48f3e379edf9f508109
abrahampasillas <abrahamp@uw.edu>
2025-12-07 02:32:00 -0800

    added microservice func

--- src/main/java/com/skyscanner/HoenScannerApplication.java
+++ src/main/java/com/skyscanner/HoenScannerApplication.java
@@ -1,8 +1,14 @@
 package com.skyscanner;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
 public class HoenScannerApplication extends Application<HoenScannerConfiguration> {
 
@@ -21,8 +27,25 @@ public class HoenScannerApplication extends Application<HoenScannerConfiguration
     }
 
     @Override
-    public void run(final HoenScannerConfiguration configuration, final Environment environment) {
-
+    public void run(final HoenScannerConfiguration configuration, final Environment environment) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        List<SearchResult> carResults = Arrays.asList(
+                mapper.readValue(
+                        getClass().getClassLoader().getResource("rental_cars.json"),
+                        SearchResult[].class
+                )
+        );
+        List<SearchResult> hotelResuls = Arrays.asList(
+                mapper.readValue(
+                        getClass().getClassLoader().getResource("hotels.json"),
+                        SearchResult[].class
+                )
+        );
+        List<SearchResult> searchResults = new ArrayList<>();
+        searchResults.addAll(carResults);
+        searchResults.addAll(hotelResuls);
+        final SearchResource resource = new SearchResource(searchResults);
+        environment.jersey().register(resource);
     }
 
 }
